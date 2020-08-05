4fde3549e87d8a4b5c5283961a27d157a4736334
Vitor Nobre <37857799+vitor-nobre@users.noreply.github.com>
2020-08-05 14:46:01 -0300

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,13 +4,13 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import TeacherList from './pages/TeacherList'
 import TeacherForm from './pages/TeacherForm'
-
+//<Route path="/give-classes" component={TeacherForm} />
 function Routes() {
     return (
         <BrowserRouter>
             <Route path="/" exact component={Landing}/>
             <Route path="/study" component={TeacherList}/>
-            <Route path="/give-classes" component={TeacherForm} />
+            
         </BrowserRouter>
     )
 }
