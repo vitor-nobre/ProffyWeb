1656bd7921db8e3bb4b374cf2f19033558e51ea6
Vitor Nobre <37857799+vitor-nobre@users.noreply.github.com>
2020-08-05 14:47:27 -0300

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import TeacherList from './pages/TeacherList'
-import TeacherForm from './pages/TeacherForm'
+//import TeacherForm from './pages/TeacherForm'
 //<Route path="/give-classes" component={TeacherForm} />
 function Routes() {
     return (
