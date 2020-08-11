cb4aa5ad86d4be1c26b0bc9a39e0d4a476691a4c
vitor <vitorn2009@hotmail.com>
2020-08-11 00:54:46 -0300

    Ajuste no componente onte mostra os dados do proffy

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,33 +1,53 @@
 import React from 'react'
-
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
-
 import './styles.css'
+import api from '../../services/api'
 
-function TeacherItem() {
+export interface Teacher {
+    "avatar": string,
+    "bio": string,
+    "cost": number,
+    "id": number,
+    "name": string,
+    "subject": string,
+    "whatsapp": string,
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function crateNewConnection() {
+        api.post('connections',{
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars3.githubusercontent.com/u/37857799?s=460&u=8e62a7665ba6942cceec6c12117363dfc1ddaa7f&v=4" alt="Vitor"/>
+                <img src={teacher.avatar} alt="Vitor"/>
                 <div>
-                    <strong>Vitor Nobre</strong>
-                    <span>Quimica</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Entusiasta das melhores tecnologias de química avançada.
-            <br></br>
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões. 
-            </p>
+            <p>{teacher.bio}</p>
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a 
+                    target="blank"
+                    onClick={crateNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
